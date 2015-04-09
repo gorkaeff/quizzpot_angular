0a4860e312dbca94484f3ec2543785eb897cd270
gorkaeff <gorkajm85@gmail.com>
2015-04-09 11:15:49 +0200

    APARTADO: Desplegar colecciones de información (ng-repeat)

--- js/app.js
+++ js/app.js
@@ -8,5 +8,12 @@
     .controller('MainController',function ($scope){
         $scope.name = 'Gorka';
         $scope.categories = ['HTML5','JavaScript','CSS','Games'];
+		$scope.bookmarks = [
+			{id:1,name:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaScript'},
+			{id:2,name:'Html5 Game Devs',url:'https://html5gamedevs.com',category:'Games'},
+			{id:3,name:'CSS Tricks',url:'http://css-tricks.com',category:'CSS'},
+			{id:4,name:'Bootstrap',url:'http://getbootstrap.com',category:'CSS'},
+			{id:5,name:'Card',url:'http://jessepollak.github.io/card/',category:'JavaScript'}
+		];
     });
 })();
