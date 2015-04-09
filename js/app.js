ded917de91599f23f5b764a9318021e83be5d48c
gorkaeff <gorkajm85@gmail.com>
2015-04-09 13:21:29 +0200

    APARTADO: Creando, editando y borrando información

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@
 
         $scope.categories = ['HTML5','JavaScript','CSS','Games'];
 
-		$scope.bookmarks = [
-			{id:1,name:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaScript'},
-			{id:2,name:'Html5 Game Devs',url:'https://html5gamedevs.com',category:'Games'},
-			{id:3,name:'CSS Tricks',url:'http://css-tricks.com',category:'CSS'},
-			{id:4,name:'Bootstrap',url:'http://getbootstrap.com',category:'CSS'},
-			{id:5,name:'Card',url:'http://jessepollak.github.io/card/',category:'JavaScript'}
-		];
+        $scope.bookmarks = [
+            {id:1,title:'Quizzpot.com',url:'https://quizzpot.com',category:'JavaScript'},
+            {id:2,title:'Html5 Game Devs',url:'https://html5gamedevs.com',category:'Games'},
+            {id:3,title:'CSS Tricks',url:'http://css-tricks.com',category:'CSS'},
+            {id:4,title:'Bootstrap',url:'http://getbootstrap.com',category:'CSS'},
+            {id:5,title:'Card',url:'http://jessepollak.github.io/card/',category:'JavaScript'}
+        ];
 
         $scope.currentCategory = 'JavaScript';
 
@@ -29,7 +29,33 @@
         }
 
         $scope.save = function(bookmark){
-            console.log(bookmark);
+            if($scope.bookmarkForm.$valid){
+                if(!bookmark.id){
+                    var record = angular.copy(bookmark);
+
+                    record.id = $scope.bookmarks.length;
+                    $scope.bookmarks.push(record);
+                }
+                $('#bookmarkModal').modal('hide');
+            }
+        }
+
+        $scope.remove = function(id){
+            for(var i=0,len=$scope.bookmarks.length;i<len;i++){
+                if($scope.bookmarks[i].id === id){
+                    $scope.bookmarks.splice(i,1);
+                    break;
+                }
+            }
+        }
+
+        $scope.showWindow = function(bookmark){
+            $scope.bookmarkForm.$setPristine();
+            $scope.bookmarkForm.$setUntouched();
+
+            bookmark = bookmark || {category:$scope.currentCategory,url:''};
+            $scope.bookmark = bookmark;       
+            $('#bookmarkModal').modal('show');
         }
     });
 })();
